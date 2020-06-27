28011f69fdd2b011dee6be5a1160d8d3feca9765
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-06-27 00:42:12 -0700

    c80

--- dye_test.go
+++ dye_test.go
@@ -6,7 +6,16 @@ import (
 
 func ExampleDye() {
 
+	// Make the entire raster image the color of color 3 in the (color) palette.
 	c80.Dye(3)
+
+	// Show the raster image.
+	//
+	// This will output something like:
+	//
+	//	IMAGE:base64-encoded-png
+	//
+	// Where "base64-encoded-png" is replaced with the base64 encoding of a PNG version of the raster image.
 	c80.Show()
 
 	// Output:
