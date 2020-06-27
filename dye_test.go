a31912a6b0286df7d29f528feb4fc85a1634b42c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-06-27 01:18:20 -0700

    c80

---
+++ dye_test.go
@@ -0,0 +1,32 @@
+package c80_test
+
+import (
+	"github.com/reiver/go-c80"
+	"github.com/reiver/go-c80/palette"
+	"github.com/reiver/go-c80/raster"
+
+	"testing"
+)
+
+func TestDye(t *testing.T) {
+
+	for index:=uint8(0); index<c80palette.Size; index++ {
+
+		testNumber := index
+
+		c80.Dye(index)
+
+		for y:=0; y<c80raster.Height; y++ {
+			for x:=0; x<c80raster.Width; x++ {
+				actual := c80.PixelGet(x,y)
+
+				if expected := index; expected != actual {
+					t.Errorf("For test #%d, actual color index at (x,y)=(%d,%d) is not what was expected.", testNumber, x,y)
+					t.Logf("EXPECTED: %d", expected)
+					t.Logf("ACTUAL:   %d", actual)
+					continue
+				}
+			}
+		}
+	}
+}
