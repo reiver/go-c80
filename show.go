7fd961cbe68de1e0bd1b47618bae7b0b0504adc1
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-06-27 10:10:24 -0700

    c80

--- show.go
+++ show.go
@@ -2,8 +2,16 @@ package c80
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
+// Show will output the raster as an "IMAGE:base64-encoded-png" string to STDOUT.
 func Show() {
-	fmt.Print(raster)
+	ShowTo(os.Stdout)
+}
+
+// ShowTo will output the raster as an "IMAGE:base64-encoded-png" string to ‘w’.
+func ShowTo(w io.Writer) {
+	fmt.Fprint(w, raster)
 }
