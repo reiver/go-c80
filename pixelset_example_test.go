a9c0012bcbea894edc9abc1dee1d40c7ea687704
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-06-27 20:56:42 -0700

    c80

---
+++ pixelset_example_test.go
@@ -0,0 +1,101 @@
+package c80_test
+
+import (
+	"github.com/reiver/go-c80"
+)
+
+func ExamplePixelSet() {
+
+	// Make the entire raster image the color of color 0 in the (color) palette.
+	c80.Dye(0)
+
+	// Draw something.
+	{
+		left := 20
+		top  := 30
+
+		//                0
+		c80.PixelSet(left+1, top+0, 6)
+		c80.PixelSet(left+2, top+0, 6)
+		c80.PixelSet(left+3, top+0, 6)
+		c80.PixelSet(left+4, top+0, 6)
+		//                5
+		//                6
+		//                7
+
+		//                0
+		//                1
+		c80.PixelSet(left+2, top+1, 6)
+		c80.PixelSet(left+3, top+1, 6)
+		c80.PixelSet(left+4, top+1, 6)
+		c80.PixelSet(left+5, top+1, 6)
+		//                6
+		//                7
+
+		c80.PixelSet(left+0, top+2, 6)
+		//                1
+		c80.PixelSet(left+2, top+2, 6)
+		c80.PixelSet(left+3, top+2, 15)
+		c80.PixelSet(left+4, top+2, 6)
+		c80.PixelSet(left+5, top+2, 15)
+		//                6
+		c80.PixelSet(left+7, top+2, 6)
+
+		c80.PixelSet(left+0, top+3, 6)
+		//                1
+		c80.PixelSet(left+2, top+3, 6)
+		c80.PixelSet(left+3, top+3, 6)
+		c80.PixelSet(left+4, top+3, 6)
+		c80.PixelSet(left+5, top+3, 6)
+		//                6
+		c80.PixelSet(left+7, top+3, 6)
+
+		//                0
+		c80.PixelSet(left+1, top+4, 6)
+		c80.PixelSet(left+2, top+4, 6)
+		c80.PixelSet(left+3, top+4, 6)
+		c80.PixelSet(left+4, top+4, 15)
+		c80.PixelSet(left+5, top+4, 6)
+		c80.PixelSet(left+6, top+4, 6)
+		//                7
+
+		//                0
+		//                1
+		c80.PixelSet(left+2, top+5, 6)
+		c80.PixelSet(left+3, top+5, 6)
+		c80.PixelSet(left+4, top+5, 6)
+		c80.PixelSet(left+5, top+5, 6)
+		//                6
+		//                7
+
+		c80.PixelSet(left+0, top+6, 6)
+		//                1
+		c80.PixelSet(left+2, top+6, 6)
+		c80.PixelSet(left+3, top+6, 6)
+		c80.PixelSet(left+4, top+6, 6)
+		//                5
+		//                6
+		//                7
+
+		//                0
+		c80.PixelSet(left+1, top+7, 6)
+		c80.PixelSet(left+2, top+7, 6)
+		c80.PixelSet(left+3, top+7, 6)
+		//                4
+		//                5
+		//                6
+		//                7
+	}
+
+	// Show the raster image.
+	//
+	// This will output something like:
+	//
+	//	IMAGE:base64-encoded-png
+	//
+	// Where "base64-encoded-png" is replaced with the base64 encoding of a PNG version of the raster image.
+	c80.Show()
+
+	// Output:
+	// IMAGE:iVBORw0KGgoAAAANSUhEUgAAAH8AAAC/BAMAAAAlex6UAAAAMFBMVEUBAQHeOCs5tUr/xwYAb7h2JnEstenMzMyAgID/AAAA/wD//wAAAP//AP8A//////9ssw/GAAAASUlEQVR4nOzRoREAMQwDwUeuM0g03T9OESIGuzw3GfkDAPaanLKQdO/PvdMFki4w+csR6g3aK0z5AQAAAAAAAAAAAFjgBQAA//8xqwgvyluaUQAAAABJRU5ErkJggg==
+}
