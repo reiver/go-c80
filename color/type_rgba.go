70720a676ed9f5cda765f0a38261f56a12723e8f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-06-27 19:00:06 -0700

    correction

--- color/type_rgba.go
+++ color/type_rgba.go
@@ -4,5 +4,5 @@ package c80color
 func (receiver Type) RGBA() (r, g, b, a uint32) {
 	const opaque = uint32(0xffff)
 
-	return uint32(receiver.red), uint32(receiver.green), uint32(receiver.blue), opaque
+	return uint32(receiver.red)<<8, uint32(receiver.green)<<8, uint32(receiver.blue)<<8, opaque
 }
