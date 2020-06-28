39af9a670aa6f5263d40718f16b9253789a1f684
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-06-28 12:55:56 -0700

    c80raster

--- machine/type.go
+++ machine/type.go
@@ -4,8 +4,16 @@ import (
 	"github.com/reiver/go-c80/palette"
 )
 
+const Len = c80palette.Len
+
 // Type represents a virtual machine that has a raster image, and a palette.
 type Type struct {
-	palette c80palette.Type
-	buffer [Width*Height]uint8
+	memory [Len]uint8
+}
+
+func (receiver Type) Palette() c80palette.Type {
+	beginning := 0
+	ending    := beginning + c80palette.Len
+
+	return memory[beginning:ending]
 }
