a88d7ea12c8618bc60be6939315eada5a04e6d1e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-06-28 11:05:25 -0700

    c80palette

--- palette/size.go
+++ palette/size.go
@@ -1,9 +1,5 @@
 package c80palette
 
-import (
-	"github.com/reiver/go-c80/color"
-)
-
 // Size represents the number of color in a palette — i.e., how many colors are in the palette.
 //
 // Note that this is different than the number of bytes that a palette takes up.
