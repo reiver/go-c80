312528d68be5b330c2a8c1c8b445847a1bf7fc2f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-06-28 02:27:17 -0700

    c80color

---
+++ color/type_peek_test.go
@@ -0,0 +1,32 @@
+package c80color_test
+
+import (
+	"github.com/reiver/go-c80/color"
+
+	"math/rand"
+	"time"
+
+	"testing"
+)
+
+func TestTypePeek(t *testing.T) {
+
+	randomness := rand.New(rand.NewSource( time.Now().UTC().UnixNano() ))
+
+	for testNumber:=0; testNumber<50; testNumber++ {
+
+		var buffer [4]uint8
+		for i:=0; i<len(buffer); i++ {
+			buffer[i] = uint8(randomness.Intn(256))
+		}
+
+		actual := c80color.Type(buffer[:]).Peek()
+
+		if expected := buffer; expected != actual {
+			t.Errorf("For test #%d, actual is not what was expected.", testNumber)
+			t.Logf("EXPECTED: %#v", expected)
+			t.Logf("ACTUAL:   %#v", actual)
+			continue
+		}
+	}
+}
