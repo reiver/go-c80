3ea2fcc0888b94198dbf34883c5b77950a5fd7e3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-06-28 02:10:09 -0700

    c80color

--- color/type_peek.go
+++ color/type_peek.go
@@ -1,6 +1,6 @@
 package c80color
 
-// Peek return the value of the color in a [4]uint8 in the following format:
+// Peek returns the color in a [4]uint8 in the following format:
 //
 //	[4]uint8{red,green,blue,alpha}
 func (receiver Type) Peek() (rgba [4]uint8) {
