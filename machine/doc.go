aaaf014f20d6886cff4d1e5357097469b2dda2f1
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-06-29 12:55:03 -0700

    c80machine

---
+++ machine/doc.go
@@ -0,0 +1,4 @@
+/*
+Package c80machine provides a type that represents a fantasy virtual machine inspired by arcade game machines, home video game consoles, handheld game consoles, and (other) computers from the 1980s.
+*/
+package c80palette
