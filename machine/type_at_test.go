822e631efbf83035fe1647d4ebb880faa8dacce5
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-06-29 14:24:54 -0700

    c80machine

--- machine/type_at_test.go
+++ machine/type_at_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/reiver/go-c80/color"
 	"github.com/reiver/go-c80/machine"
 	"github.com/reiver/go-c80/palette"
+	"github.com/reiver/go-c80/raster"
 
 	"math/rand"
 	"time"
@@ -15,39 +16,78 @@ func TestTypeAt(t *testing.T) {
 
 	randomness := rand.New(rand.NewSource( time.Now().UTC().UnixNano() ))
 
-	var palette [c80palette.Size]c80color.Type
-	for index:=0; index<c80palette.Size; index++ {
-		palette[index] = c80color.RGB(
+	var machine c80machine.Type
+
+	for index:=uint8(0); index<c80palette.Size; index++ {
+		machine.Palette().Color(index).Poke(
 			uint8(randomness.Intn(256)),
 			uint8(randomness.Intn(256)),
 			uint8(randomness.Intn(256)),
+			uint8(255),
 		)
 	}
 
 	for index:=uint8(0); index<c80palette.Size; index++ {
+		actual := machine.Palette().Color(index).Peek()
+		notExpected := c80color.Array{}
 
-		testNumber := index
-
-		var machine c80machine.Type
-		machine.SetPalette(c80palette.RGBs(palette[:]...))
-		machine.Dye(index)
-
-		for y:=0; y<c80machine.Height; y++ {
-			for x:=0; x<c80machine.Width; x++ {
-
-				color := machine.At(x,y)
-
-				ar,ag,ab,aa := color.RGBA()
-
-				er,eg,eb,ea := palette[index].RGBA()
-
-				if er != ar || eg != ag || eb != ab || ea != aa {
-					t.Errorf("For test #%d, actual color for pixel (x,y)=(%d,%d) was not what was expected.", testNumber, x,y)
-					t.Logf("EXPECTED: (r,g,b,a)=(%d,%d,%d,%d)", er,eg,eb,ea)
-					t.Logf("ACTUAL:   (r,g,b,a)=(%d,%d,%d,%d)", ar,ag,ab,aa)
-				}
-			}
+		if notExpected == actual {
+			t.Errorf("Was not able to set color №%d in palette.", index)
+			t.Log("NOT EXPECTED:", notExpected)
+			t.Log("ACTUAL:      ", actual)
+			continue
 		}
 	}
 
+	for y:=0; y<c80raster.Height; y++ {
+		for x:=0; x<c80raster.Width; x++ {
+
+			index := uint8(randomness.Intn(16))
+
+			machine.Raster(0).SetColorIndex(x, y, index)
+		}
+	}
+
+	{
+		numNotZero := 0
+
+		for y:=0; y<c80raster.Height; y++ {
+			for x:=0; x<c80raster.Width; x++ {
+
+				index := machine.Raster(0).ColorIndexAt(x,y)
+
+				if 0 != index {
+					numNotZero++
+				}
+			}
+		}
+
+		if 0 == numNotZero {
+			t.Errorf("Was not able to set color of pixels on raster №0.")
+			t.Log("Number of non-zero pixels:", numNotZero)
+			return
+		}
+	}
+
+	for testNumber:=0; testNumber<50; testNumber++ {
+		x := randomness.Intn(c80raster.Width)
+		y := randomness.Intn(c80raster.Height)
+
+		index := uint8(randomness.Intn(c80palette.Size))
+		color := machine.Palette().Color(index)
+
+		er,eg,eb,ea := color.RGBA()
+
+		machine.Raster(0).SetColorIndex(x,y,index)
+
+		ar,ag,ab,aa := machine.At(x,y).RGBA()
+
+		if er != ar || eg != ag || eb != ab || ea != aa {
+			t.Errorf("For test #%d, actual RGBA is not what was actually expected.", testNumber)
+			t.Logf("EXPECTED: RGBA16(%d,%d,%d,%d)", er,eg,eb,ea)
+			t.Logf("ACTUAL:   RGBA16(%d,%d,%d,%d)", ar,ag,ab,aa)
+			continue
+		}
+	}
 }
+
