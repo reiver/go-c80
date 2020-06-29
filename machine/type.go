ef47d7b9a00a15e06d8f360d08d7699a703883f4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-06-29 12:56:23 -0700

    c80machine

--- machine/type.go
+++ machine/type.go
@@ -2,14 +2,11 @@ package c80machine
 
 import (
 	"github.com/reiver/go-c80/palette"
-	"github.com/reiver/go-c80/raster"
 )
 
-const NumberOfRasters = 3
+const Len = c80palette.Len
 
-const Len = c80palette.Len + NumberOfRasters*c80raster.Len
-
-// Type represents a virtual machine that has a raster image, and a palette.
+// Type represents a fantasy virtual machine.
 type Type struct {
 	memory [Len]uint8
 }
@@ -17,15 +14,6 @@ type Type struct {
 func (receiver Type) Palette() c80palette.Type {
 	beginning := 0
 	ending    := beginning + c80palette.Len
-
-	return receiver.memory[beginning:ending]
-}
-
-func (receiver Type) Raster(number int) c80raster.Type {
-	number = number % NumberOfRasters
-
-	beginning := c80palette.Len + number*c80raster.Len
-	ending    := beginning + c80raster.Len
 
 	return receiver.memory[beginning:ending]
 }
