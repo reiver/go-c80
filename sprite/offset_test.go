10b1eed940734f722aca8fdfbca0b494774bf098
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-06-30 13:08:51 -0700

    c80sprite

---
+++ sprite/offset_test.go
@@ -0,0 +1,244 @@
+package c80sprite_test
+
+import (
+	"github.com/reiver/go-c80/sprite"
+
+	"testing"
+)
+
+func TestOffset(t *testing.T) {
+
+	tests := []struct{
+		X int
+		Y int
+		Expected int
+	}{
+		{
+			X:         0,
+			Y:         0,
+			Expected:  0,
+		},
+		{
+			X:         1,
+			Y:         0,
+			Expected:  1,
+		},
+		{
+			X:         2,
+			Y:         0,
+			Expected:  2,
+		},
+		{
+			X:         3,
+			Y:         0,
+			Expected:  3,
+		},
+		{
+			X:         4,
+			Y:         0,
+			Expected:  4,
+		},
+		{
+			X:         5,
+			Y:         0,
+			Expected:  5,
+		},
+		{
+			X:         6,
+			Y:         0,
+			Expected:  6,
+		},
+		{
+			X:         7,
+			Y:         0,
+			Expected:  7,
+		},
+
+
+
+		{
+			X:         0,
+			Y:         1,
+			Expected:  8,
+		},
+		{
+			X:         1,
+			Y:         1,
+			Expected:  9,
+		},
+		{
+			X:         2,
+			Y:         1,
+			Expected:  10,
+		},
+		{
+			X:         3,
+			Y:         1,
+			Expected:  11,
+		},
+		{
+			X:         4,
+			Y:         1,
+			Expected:  12,
+		},
+		{
+			X:         5,
+			Y:         1,
+			Expected:  13,
+		},
+		{
+			X:         6,
+			Y:         1,
+			Expected:  14,
+		},
+		{
+			X:         7,
+			Y:         1,
+			Expected:  15,
+		},
+
+
+
+		{
+			X:         0,
+			Y:         2,
+			Expected:  16,
+		},
+		{
+			X:         1,
+			Y:         2,
+			Expected:  17,
+		},
+		{
+			X:         2,
+			Y:         2,
+			Expected:  18,
+		},
+		{
+			X:         3,
+			Y:         2,
+			Expected:  19,
+		},
+		{
+			X:         4,
+			Y:         2,
+			Expected:  20,
+		},
+		{
+			X:         5,
+			Y:         2,
+			Expected:  21,
+		},
+		{
+			X:         6,
+			Y:         2,
+			Expected:  22,
+		},
+		{
+			X:         7,
+			Y:         2,
+			Expected:  23,
+		},
+
+
+
+		{
+			X:         0,
+			Y:         3,
+			Expected:  24,
+		},
+		{
+			X:         1,
+			Y:         3,
+			Expected:  25,
+		},
+		{
+			X:         2,
+			Y:         3,
+			Expected:  26,
+		},
+		{
+			X:         3,
+			Y:         3,
+			Expected:  27,
+		},
+		{
+			X:         4,
+			Y:         3,
+			Expected:  28,
+		},
+		{
+			X:         5,
+			Y:         3,
+			Expected:  29,
+		},
+		{
+			X:         6,
+			Y:         3,
+			Expected:  30,
+		},
+		{
+			X:         7,
+			Y:         3,
+			Expected:  31,
+		},
+
+
+
+		{
+			X:         0,
+			Y:         4,
+			Expected:  32,
+		},
+		{
+			X:         1,
+			Y:         4,
+			Expected:  33,
+		},
+		{
+			X:         2,
+			Y:         4,
+			Expected:  34,
+		},
+		{
+			X:         3,
+			Y:         4,
+			Expected:  35,
+		},
+		{
+			X:         4,
+			Y:         4,
+			Expected:  36,
+		},
+		{
+			X:         5,
+			Y:         4,
+			Expected:  37,
+		},
+		{
+			X:         6,
+			Y:         4,
+			Expected:  38,
+		},
+		{
+			X:         7,
+			Y:         4,
+			Expected:  39,
+		},
+	}
+
+	for testNumber, test := range tests {
+
+		actual := c80sprite.Offset(test.X, test.Y)
+
+		expected := test.Expected
+
+		if expected != actual {
+			t.Errorf("For test #%d, actual value was not what was expected.", testNumber)
+			t.Logf("(x,y) = (%d, %d)", test.X, test.Y)
+			t.Logf("EXPECTED: %d", expected)
+			t.Logf("ACTUAL:   %d", actual)
+			continue
+		}
+	}
+}
