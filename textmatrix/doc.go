4bb991905e825dd7ff032b13ab8994f112b13003
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-13 00:44:20 -0700

    c80textmatrix

---
+++ textmatrix/doc.go
@@ -0,0 +1,8 @@
+/*
+Package c80textmatrix provides a type for dealing with a text matrix.
+
+A text matrix is a 2-dimensional matrix of UNICODE characters.
+
+In particular, it 32×32 matrix of UNICODE characters.
+*/
+package c80textmatrix
