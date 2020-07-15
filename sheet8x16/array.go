da9202914a8a6c432a6d1b6bb44990203b9ad764
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-14 20:00:49 -0700

    removed c80sheet8x16

--- sheet8x16/array.go
+++
@@ -1,3 +0,0 @@
-package c80sheet8x16
-
-type Array [Len]uint8
--- sheet8x16/doc.go
+++
@@ -1,4 +0,0 @@
-/*
-Package c80sheet8x16 provides types that represent a sprite sheet made up of 8x16 pixel sprites.
-*/
-package c80sheet8x16
