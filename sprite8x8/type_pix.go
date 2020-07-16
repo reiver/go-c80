d362f167f18dd94ab895c8559ce7c357e1fe628a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-16 13:24:48 -0700

    sprite8x8

---
+++ sprite8x8/type_pix.go
@@ -0,0 +1,5 @@
+package c80sprite8x8
+
+func (receiver Type) Pix() []uint8 {
+	return receiver.bytes
+}
---
+++ sprite8x8/type_stride.go
@@ -0,0 +1,5 @@
+package c80sprite8x8
+
+func (receiver Type) Stride() int {
+	return Width*Depth
+}
