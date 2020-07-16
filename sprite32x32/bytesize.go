1a7db779f96bb848e464a3c9c26c822a79389d7a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-16 13:26:11 -0700

    sprite32x32

--- sprite32x32/bytesize.go
+++ sprite32x32/bytesize.go
@@ -1,11 +1,7 @@
 package c80sprite32x32
 
-import (
-	"github.com/reiver/go-c80/pixel"
-)
-
 // ByteSize represents the number of bytes in a sprite.
 //
 // Each sprite is 32×32 pixels; which means there are 1024 pixels total.
 // Each pixel is 1 byte. Therefore the number of bytes in a sprite is 1024.
-const ByteSize = Width*Height * c80pixel.ByteSize
+const ByteSize = Width*Height*Depth
--- sprite32x32/dimensions.go
+++ sprite32x32/dimensions.go
@@ -1,6 +1,11 @@
 package c80sprite32x32
 
+import (
+	"github.com/reiver/go-c80/pixel"
+)
+
 const (
 	Width  = 32
 	Height = 32
+	Depth = c80pixel.ByteSize
 )
