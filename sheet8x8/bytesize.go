5888a67e3e88c6f094210a8f988da690e9157c14
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-16 13:51:04 -0700

    sheet8x8

--- sheet8x8/bytesize.go
+++ sheet8x8/bytesize.go
@@ -4,4 +4,4 @@ import (
 	"github.com/reiver/go-c80/sprite8x8"
 )
 
-const ByteSize = 256 * c80sprite8x8.ByteSize
+const ByteSize = Len * c80sprite8x8.ByteSize
---
+++ sheet8x8/len.go
@@ -0,0 +1,4 @@
+package c80sheet8x8
+
+// Len represents the number of sprites in a sheet.
+const Len = 256
