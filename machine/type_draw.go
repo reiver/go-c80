7a51f5c4f2792fcb9406242bb21bd11e82c39f04
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-16 01:34:25 -0700

    machine

--- machine/type_draw.go
+++ machine/type_draw.go
@@ -3,15 +3,57 @@ package c80machine
 import (
 	"github.com/reiver/go-font8x8"
 
+	"github.com/reiver/go-c80/palettedraster"
 	"github.com/reiver/go-c80/textmatrix"
 
 	"image"
 	"image/draw"
 )
 
+func (receiver *Type) DrawRaster() {
+	if nil == receiver {
+		return
+	}
+
+	dst := receiver.frame.DrawableImage()
+	if nil == dst {
+		return
+	}
+
+	var rect image.Rectangle
+	{
+		const x = 0
+		const y = 0
+
+		rect = image.Rectangle{
+			Min: image.Point{
+				X:x,
+				Y:y,
+			},
+			Max: image.Point{
+				X:x+c80palettedraster.Width,
+				Y:y+c80palettedraster.Height,
+			},
+		}
+	}
+
+	var src image.Image
+	{
+		palettedraster := receiver.PalettedRaster()
+		if palettedraster.IsNothing() {
+			return
+		}
+
+		src = palettedraster.DrawableImage()
+		if nil == src {
+			return
+		}
+	}
+
+	draw.Draw(dst, rect, src, image.ZP, draw.Src)
+}
+
 func (receiver *Type) DrawTextMatrix() {
-
-
 	if nil == receiver {
 		return
 	}
