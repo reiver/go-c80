a26fabd175ac94fc7a481e8bdfb96211f7f9177f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-16 00:25:46 -0700

    machine

--- machine/type_string.go
+++ machine/type_string.go
@@ -1,27 +1,5 @@
 package c80machine
 
-import (
-	"bytes"
-	"encoding/base64"
-	"image/png"
-	"strings"
-)
-
 func (receiver Type) String() string {
-	var buffer strings.Builder
-
-	{
-		buffer.WriteString("IMAGE:")
-	}
-
-	{
-		var pngBuffer bytes.Buffer
-
-		png.Encode(&pngBuffer, receiver)
-
-		base64Encoded := base64.StdEncoding.EncodeToString(pngBuffer.Bytes())
-		buffer.WriteString(base64Encoded)
-	}
-
-	return buffer.String()
+	return receiver.frame.String()
 }
