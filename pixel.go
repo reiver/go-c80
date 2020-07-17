4abfa6a0b9d3c57265e05fb278eeb0ef5736ed60
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-16 21:28:17 -0700

    c80

--- pixel.go
+++ pixel.go
@@ -1,15 +1,5 @@
 package c80
 
-// PixelSet sets the color of the pixel at (x,y)
-// to the color represented by the index into the
-// (color) palette.
-func PixelSet(x int, y int, index uint8) {
-	machine.Raster0().SetColorIndex(x,y,index)
-}
-
-// PixelGet gets the color of the pixel at (x,y)
-// as the color represented by the index into the
-// (color) palette.
-func PixelGet(x int, y int) uint8 {
-	return machine.Raster0().ColorIndexAt(x,y)
+func DrawPixel(x int, y int, index uint8) {
+	machine.DrawPixel(x,y, index)
 }
