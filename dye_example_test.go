d8bf18c60c3b8063824b37ba462019995be427cf
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-17 00:41:54 -0700

    c80

--- dye_example_test.go
+++ dye_example_test.go
@@ -2,6 +2,8 @@ package c80_test
 
 import (
 	"github.com/reiver/go-c80"
+
+	"fmt"
 )
 
 func ExampleDye() {
@@ -16,7 +18,8 @@ func ExampleDye() {
 	//	IMAGE:base64-encoded-png
 	//
 	// Where "base64-encoded-png" is replaced with the base64 encoding of a PNG version of the raster image.
-	c80.Show()
+	serialized := c80.Serialize()
+	fmt.Print(serialized)
 
 	// Output:
 	// IMAGE:iVBORw0KGgoAAAANSUhEUgAAAH8AAAC/BAMAAAAlex6UAAAAMFBMVEUBAQHeOCs5tUr/xwYAb7h2JnEstenMzMyAgID/AAAA/wD//wAAAP//AP8A//////9ssw/GAAAATElEQVR4nOzMQQ0AAAgEICO52b+bIe53gwDMZVYgEAgEAoFAIBAIBAKBQCAQCAQCgUAgEAgEAoFAIBAIBAKBQCAQCAQCQXvwAQAA///AzoGLUCkA2wAAAABJRU5ErkJggg==
