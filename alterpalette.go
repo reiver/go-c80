1a110059746917fee62750329754f48d36c50c03
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-17 19:18:21 -0700

    c80

---
+++ alterpalette.go
@@ -0,0 +1,50 @@
+package c80
+
+import (
+	"github.com/reiver/go-c80/palette"
+)
+
+func AlterPalette(a ...interface{}) {
+
+	if nil == a {
+		return
+	}
+	if 0 >= len(a) {
+		return
+	}
+
+	if 1 < len(a) {
+		return
+	}
+
+	a0 := a[0]
+
+	s0, casted := a0.(string)
+	if !casted {
+		return
+	}
+
+	var p []byte
+	{
+		switch s0 {
+		case "tia128":
+			p = palette_tia128[:]
+		}
+	}
+	if nil == p {
+		return
+	}
+	if 0 >= len(p) {
+		return
+	}
+	if c80palette.ByteSize != len(p) {
+		return
+	}
+
+	palette := machine.Palette()
+	if palette.IsNothing() {
+		return
+	}
+
+	palette.Replace(p...)
+}
