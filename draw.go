5e328f485672af76fbff5688289ee80f5fe65f2a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-19 21:26:29 -0700

    c80

--- draw.go
+++ draw.go
@@ -32,18 +32,5 @@ import (
 //
 //	err := c80.Draw(c80.Text("Hello world!"), x, y)
 func Draw(img image.Image) error {
-	switch casted := img.(type) {
-	case c80dye.Type:
-		return machine.DrawDye(casted)
-	case *c80dye.Type:
-		return machine.DrawDye(casted)
-
-	case pel.RGBA:
-		return machine.DrawPixel(casted.X, casted.Y, casted)
-	case *pel.RGBA:
-		return machine.DrawPixel(casted.X, casted.Y, casted)
-
-	default:
-		return machine.Draw(img)
-	}
+	return machine.Draw(img)
 }
