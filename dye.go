6887bab99105e92e07ac98a8b8679590a9b41aea
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-20 14:30:15 -0700

    c80

--- dye.go
+++ dye.go
@@ -1,8 +1,6 @@
 package c80
 
 import (
-	"github.com/reiver/go-c80/dye"
-
 	"github.com/reiver/go-rgba32"
 
 	"image"
@@ -11,10 +9,5 @@ import (
 func Dye(index uint8) image.Image {
 	rgba := machine.Palette().Color(index).(rgba32.Slice)
 
-	return c80dye.Type{
-		R: rgba[rgba32.OffsetRed],
-		G: rgba[rgba32.OffsetGreen],
-		B: rgba[rgba32.OffsetBlue],
-		A: rgba[rgba32.OffsetAlpha],
-	}
+	return rgba
 }
