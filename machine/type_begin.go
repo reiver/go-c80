05da8322d28dd910092b4fbb7bf5d193ca080519
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-19 18:50:05 -0700

    c80

--- machine/type_begin.go
+++
@@ -1,9 +0,0 @@
-package c80machine
-
-func (receiver *Type) Begin() error {
-	if nil == receiver {
-		return errNilReceiver
-	}
-
-	return nil
-}
--- machine/type_end.go
+++
@@ -1,9 +0,0 @@
-package c80machine
-
-func (receiver *Type) End() error {
-	if nil == receiver {
-		return errNilReceiver
-	}
-
-	return nil
-}
