4ad99d9ce302054b93fe34ddb02ca581450cf1da
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-24 10:23:19 -0700

    sprites including (map) tiles

--- machine/type_sprite.go
+++ machine/type_sprite.go
@@ -5,6 +5,15 @@ import (
 	"image/draw"
 )
 
+// Sprite returns a sprite as a Go built-in image.Image.
+//
+// There are 3 kinds of sprites:
+//
+// • 8x8
+// • 32x32
+// • tile
+//
+// For each fo these, the id runs from 0 to 255.
 func (receiver *Type) Sprite(kind string, id uint8) image.Image {
 	if nil == receiver {
 		return nil
@@ -15,11 +24,22 @@ func (receiver *Type) Sprite(kind string, id uint8) image.Image {
 		return receiver.Sprites8x8().Sprite(id)
 	case "32x32":
 		return receiver.Sprites32x32().Sprite(id)
+	case "tile":
+		return receiver.Tiles().Sprite(id)
 	default:
 		return nil
 	}
 }
 
+// SetSprite sets the value of a sprite to the built-in image.Image.
+//
+// There are 3 kinds of sprites:
+//
+// • 8x8
+// • 32x32
+// • tile
+//
+// For each fo these, the id runs from 0 to 255.
 func (receiver *Type) SetSprite(kind string, id uint8, img image.Image) error {
 	if nil == receiver {
 		return errNilReceiver
@@ -32,6 +52,8 @@ func (receiver *Type) SetSprite(kind string, id uint8, img image.Image) error {
 		dst = receiver.Sprites8x8().Sprite(id)
 	case "32x32":
 		dst = receiver.Sprites32x32().Sprite(id)
+	case "tile":
+		dst = receiver.Tiles().Sprite(id)
 	default:
 		return errSpriteKindNotAllowed
 	}
--- sprite.go
+++ sprite.go
@@ -4,10 +4,28 @@ import (
 	"image"
 )
 
+// Sprite returns a sprite as a Go built-in image.Image.
+//
+// There are 3 kinds of sprites:
+//
+// • 8x8
+// • 32x32
+// • tile
+//
+// For each fo these, the id runs from 0 to 255.
 func Sprite(kind string, id uint8) image.Image {
 	return machine.Sprite(kind, id)
 }
 
+// SetSprite sets the value of a sprite to the built-in image.Image.
+//
+// There are 3 kinds of sprites:
+//
+// • 8x8
+// • 32x32
+// • tile
+//
+// For each fo these, the id runs from 0 to 255.
 func SetSprite(kind string, id uint8, img image.Image) error {
 	return machine.SetSprite(kind, id, img)
 }
