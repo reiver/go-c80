d830270eb045dad30842610330c7755c4534b990
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-26 09:19:16 -0700

    docs

--- frame.go
+++ frame.go
@@ -1,5 +1,11 @@
 package c80
 
+// Frame returns a []uint8 that represents the memory that backs
+// the (main) animation frame.
+//
+// The bytes in this represents a series of 32-bit RGBA values,
+// starting from the top-left corner, and going to the bottom-right
+// corner.
 func Frame() []uint8 {
 	return machine.Frame()
 }
--- image.go
+++ image.go
@@ -4,6 +4,7 @@ import(
 	"image/draw"
 )
 
+// Image return a draw.Image for the (main) animation frame.
 func Image() draw.Image {
 	return machine.Image()
 }
