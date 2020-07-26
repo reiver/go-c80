8e89ce551beeb061c2dad13c07aac44b57777ec3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-26 09:34:53 -0700

    Memory()

--- memory.go
+++ memory.go
@@ -1,5 +1,8 @@
 package c80
 
+// Memory returns the entire memory of the c80 machine.
+//
+// Reading and writing to this give you access to everything.
 func Memory() []uint8 {
 	return machine.Memory()
 }
