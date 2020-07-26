6c1ae718876378cd32eed437d825c6da90878871
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-26 09:43:58 -0700

    docs

--- colorize.go
+++ colorize.go
@@ -7,6 +7,26 @@ package c80
 // Here are some examples of Colorize being used:
 //
 //	err := c80.Colorize("tia")
+//
+// Valid color palettes include:
+//
+// • "gb"
+//
+// • "greer"
+//
+// • "gruvbox"
+//
+// • "html3"
+//
+// • "lospec500"
+//
+// • "nes"
+//
+// • "rkbv"
+//
+// • "solarized"
+//
+// • "tia"
 func Colorize(a ...interface{}) error {
 	return machine.Colorize(a...)
 }
