db082b6f396419561ad44997a6158a71691e72aa
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-26 08:50:57 -0700

    dast8x8

--- machine.go
+++ machine.go
@@ -1,6 +1,8 @@
 package c80
 
 import (
+	"github.com/reiver/go-dast8x8"
+
 	"github.com/reiver/go-c80/machine"
 )
 
@@ -11,5 +13,17 @@ var (
 func init() {
 	if err := Colorize("vt"); nil != err {
 		panic(err)
+	}
+
+	{
+		const diff = 247
+
+		for id:=uint8(0); id<2; id++ {
+			localID := id+diff
+
+			if err := SetSprite("8x8", localID, dast8x8.Sprite(id)); nil != err {
+				panic(err)
+			}
+		}
 	}
 }
